810bc45cd7f4cdc03c893277b0f31274a0909ebf
Hafiz Muhammad Moaz <hafizmoazkhalid@gmail.com>
2026-04-30 00:21:45 +0500

    RELEASE v1.0.0

---
+++ src/Date.php
@@ -0,0 +1,133 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Webisters Date Library.
+ *
+ * (c) Hafiz Muhammad Moaz <thewebisters@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Date;
+
+use DateTime;
+use DateTimeImmutable;
+use DateTimeZone;
+use Exception;
+use Framework\Language\Language;
+use JsonSerializable;
+use Stringable;
+
+/**
+ * Class Date.
+ *
+ * @see https://www.php.net/manual/en/intldateformatter.format.php#106070
+ * @see https://www.php.net/manual/en/intldateformatter.format.php#refsect1-intldateformatter.format-changelog
+ * @see https://www.php.net/manual/en/function.strftime.php
+ *
+ * @package date
+ */
+class Date extends DateTime implements JsonSerializable, Stringable
+{
+    public const DATETIME = 'Y-m-d H:i:s';
+    protected Language $language;
+
+    final public function __construct(
+        string $datetime = 'now',
+        ?DateTimeZone $timezone = null,
+        ?Language $language = null
+    ) {
+        parent::__construct($datetime, $timezone);
+        if ($language) {
+            $this->setLanguage($language);
+        }
+    }
+
+    public function __toString() : string
+    {
+        return $this->format(static::ATOM);
+    }
+
+    public function setLanguage(Language $language) : static
+    {
+        $this->language = $language;
+        $this->language->addDirectory(__DIR__ . '/Languages');
+        return $this;
+    }
+
+    public function getLanguage() : Language
+    {
+        if (!isset($this->language)) {
+            $this->setLanguage(new Language());
+        }
+        return $this->language;
+    }
+
+    public function humanize() : string
+    {
+        $timeDifference = $this->getTimestamp() - \time();
+        $language = $this->getLanguage();
+        $condition = [
+            12 * 30 * 24 * 60 * 60 => 'years',
+            30 * 24 * 60 * 60 => 'months',
+            7 * 24 * 60 * 60 => 'weeks',
+            24 * 60 * 60 => 'days',
+            60 * 60 => 'hours',
+            60 => 'minutes',
+            1 => 'seconds',
+        ];
+        $timing = 'in';
+        if ($timeDifference < 0) {
+            $timeDifference *= -1;
+            $timing = 'ago';
+        }
+        foreach ($condition as $seconds => $line) {
+            $diff = $timeDifference / $seconds;
+            if ($diff >= 1) {
+                $arg = \round($diff);
+                return $language->render('date', $timing, [
+                    $language->render('date', $line, [$arg]),
+                ]);
+            }
+        }
+        return $language->render('date', 'now');
+    }
+
+    public function jsonSerialize() : string
+    {
+        return $this->format(static::ATOM);
+    }
+
+    /**
+     * Parse a string into a new static object according to the specified format.
+     *
+     * @param string $format Format accepted by date()
+     * @param string $datetime A string representing the time
+     * @param DateTimeZone|null $timezone A DateTimeZone object representing the
+     * desired time zone
+     *
+     * @throws Exception Emits Exception in case of an error
+     *
+     * @return false|static
+     */
+    public static function createFromFormat(
+        string $format,
+        string $datetime,
+        ?DateTimeZone $timezone = null
+    ) : false | static {
+        $object = parent::createFromFormat($format, $datetime, $timezone);
+        return $object ? new static($object->format(static::ATOM)) : $object;
+    }
+
+    /**
+     * @param DateTimeImmutable $object
+     *
+     * @throws Exception Emits Exception in case of an error
+     *
+     * @return static
+     */
+    public static function createFromImmutable(DateTimeImmutable $object) : static
+    {
+        $object = parent::createFromImmutable($object);
+        return new static($object->format(static::ATOM));
+    }
+}
---
+++ tests/DateTest.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * This file is part of Webisters Date Library.
+ *
+ * (c) Hafiz Muhammad Moaz <thewebisters@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Date;
+
+use Framework\Date\Date;
+use Framework\Language\Language;
+use PHPUnit\Framework\TestCase;
+
+final class DateTest extends TestCase
+{
+    protected Date $date;
+
+    public function setup() : void
+    {
+        $this->date = new Date();
+    }
+
+    public function testCreateFrom() : void
+    {
+        self::assertInstanceOf(
+            Date::class,
+            Date::createFromFormat('Y-m-d H:i:s', '2019-07-12 22:46:20')
+        );
+        self::assertInstanceOf(
+            Date::class,
+            Date::createFromImmutable(new \DateTimeImmutable())
+        );
+    }
+
+    public function testJsonSerialize() : void
+    {
+        self::assertSame(\json_encode(\date(\DATE_ATOM)), \json_encode($this->date));
+    }
+
+    public function testToString() : void
+    {
+        self::assertSame(\date(\DATE_ATOM), (string) $this->date);
+    }
+
+    public function testConstants() : void
+    {
+        self::assertSame(\date('Y-m-d H:i:s'), $this->date->format($this->date::DATETIME));
+    }
+
+    public function testInstance() : void
+    {
+        $date = new Date();
+        $date = $date->setDate(2019, 12, 24);
+        self::assertInstanceOf(Date::class, $date);
+        $date = $date->setTime(15, 15);
+        self::assertInstanceOf(Date::class, $date);
+    }
+
+    /**
+     * @dataProvider humanizeProvider
+     *
+     * @param string $datetime
+     * @param string $text
+     */
+    public function testHumanize(string $datetime, string $text) : void
+    {
+        $date = new Date($datetime);
+        self::assertSame($text, $date->humanize());
+    }
+
+    public function testHumanizeLocalized() : void
+    {
+        $date = new Date('+10 minutes', language: new Language('pt-br'));
+        self::assertSame('em 10 minutos', $date->humanize());
+    }
+
+    /**
+     * @return array<array<string>>
+     */
+    public static function humanizeProvider() : array
+    {
+        return [
+            ['now', 'just now'],
+            ['-1 seconds', '1 second ago'],
+            ['+1 seconds', 'in 1 second'],
+            ['-3 seconds', '3 seconds ago'],
+            ['+3 seconds', 'in 3 seconds'],
+            ['-1 minutes', '1 minute ago'],
+            ['+1 minutes', 'in 1 minute'],
+            ['-3 minutes', '3 minutes ago'],
+            ['+3 minutes', 'in 3 minutes'],
+            ['-1 hours', '1 hour ago'],
+            ['+1 hours', 'in 1 hour'],
+            ['-3 hours', '3 hours ago'],
+            ['+3 hours', 'in 3 hours'],
+            ['-1 days', '1 day ago'],
+            ['+1 days', 'in 1 day'],
+            ['-3 days', '3 days ago'],
+            ['+3 days', 'in 3 days'],
+            ['-1 weeks', '1 week ago'],
+            ['+1 weeks', 'in 1 week'],
+            ['-3 weeks', '3 weeks ago'],
+            ['+3 weeks', 'in 3 weeks'],
+            ['-2 months', '2 months ago'],
+            ['+2 months', 'in 2 months'],
+            ['-3 months', '3 months ago'],
+            ['+3 months', 'in 3 months'],
+            ['-1 years', '1 year ago'],
+            ['+1 years', 'in 1 year'],
+            ['-3 years', '3 years ago'],
+            ['+3 years', 'in 3 years'],
+        ];
+    }
+}
